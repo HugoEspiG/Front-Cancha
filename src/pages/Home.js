9659ccbf58fc26140c66d3f79187038cc2d9bf61
HugoEspiG <hugo.espinosa01@correo.usa.edu.co>
2022-09-04 02:38:12 -0500

    Generacion ReactRouter + añadir Bootstrap + Generacion components

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar"
+import Login from "../components/Login"
+
+export default function Home(){
+
+    return(
+        <div>
+            <NavBar/>
+            <Login/>
+        </div>
+    )
+}
